d05e84c82dfba67d20cf48af7657b3f94130622c
khoa208 <khoadodev@gmail.com>
2024-05-15 22:23:37 -0700

    edit

---
+++ src/components/Skills.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { skills } from "../data";
+
+export default function Skills() {
+    return (
+        <section id="skills">
+            <div className="container px-5 py-10 mx-auto">
+                <div className="text-center mb-20">
+
+                    <h1 className="sm:text-4xl text-3xl font-medium title-font text-white mb-4">
+                        Skills &amp; Technologies
+                    </h1>
+                    <p className="text-base leading-relaxed xl:w-2/4 lg:w-3/4 mx-auto">
+                        As a frontend web developer, I am proficient in creating dynamic and responsive user interfaces using JavaScript, React, CSS, HTML, and Tailwind CSS. My expertise in these technologies allows me to build seamless and intuitive web experiences, ensuring both visual appeal and functionality. With a strong focus on modern development practices, I deliver high-quality web applications that provide exceptional user experiences.
+                    </p>
+                </div>
+                <div className="flex flex-wrap lg:w-4/5 sm:mx-auto sm:mb-2 -mx-2">
+                    {skills.map((skill) => (
+                        <div key={skill} className="p-2 sm:w-1/2 w-full">
+                            <div className="bg-gray-800 rounded flex p-4 h-full items-center">
+                                <span className="title-font font-medium text-white">
+                                    {skill}
+                                </span>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </section>
+    );
+}
---
+++ src/data.js
@@ -0,0 +1,19 @@
+export const projects = [
+    {
+        title: "Simple Quizz",
+        subtitle: "10 Simple Quizz Collection",
+        description:
+            "Try yourself and see how you score",
+        image: "quizz.jpg",
+        link: "https://github.com/khoa208/349-Front-End-Project.git",
+    }
+];
+
+export const skills = [
+    "JavaScript",
+    "HTML",
+    "CSS",
+    "React",
+    "Tailwind CSS",
+    "Node.js"
+]
